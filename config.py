d1707f9d29de3425e62052592846e7bde4fb3496
DingAllen <1121174884@qq.com>
2022-08-01 20:58:39 +0800

    初次提交，项目初始化。

---
+++ config.py
@@ -0,0 +1,21 @@
+# 配置项目所需参数
+class HyperParameters:
+    # 路径配置
+    a = 1
+
+    # 数据配置
+    mosaic = True
+    mosaic_prob = 0.5
+    mixup = True
+    mixup_prob = 0.5
+    special_aug_ratio = 0.7
+
+    # 模型配置
+    input_shape = (640, 640) # (height, width)
+
+    # 训练配置
+
+
+
+
+HP = HyperParameters()
---
+++ utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def load_meta(meta_path):
+    with open(meta_path, 'r') as fr:
+        return [line for line in fr.readlines()]
+
+
+def cvtColor(image):
+    '''
+    将图像转换成RGB图像，防止灰度图在预测时报错。
+    代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+
+    :param image: PIL格式的图片数据
+    :return: 转换后的图片数据
+    '''
+    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
+        return image
+    else:
+        image = image.convert('RGB')
+        return image
+
+
+if __name__ == '__main__':
+    from config import HP
+
+    h, w = HP.input_size
+    print(h)
+    print(w)
