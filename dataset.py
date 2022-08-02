1a0735e82c54d5d58ea5a1d71f1c4291615731b4
DingAllen <1121174884@qq.com>
2022-08-02 13:01:11 +0800

    实现parse_metadata_with_mosaic函数

--- dataset.py
+++ dataset.py
@@ -1,9 +1,10 @@
 from torch.utils.data.dataset import Dataset
 import numpy as np
-from utils import load_meta, cvtColor
-from config import HP
 from PIL import Image
 import cv2
+from random import sample, shuffle
+from utils import load_meta, cvtColor
+from config import HP
 
 
 class YoloDataset(Dataset):
@@ -24,7 +25,9 @@ class YoloDataset(Dataset):
     def __getitem__(self, index):
 
         if self.isTraining and HP.mosaic and self.rand() < HP.mosaic_prob:
-            pass
+            img, box = self.parse_metadata_with_mosaic(index)
+        else:
+            img, box = self.parse_metadata(index, random=self.isTraining)
         pass
 
     def rand(self, bottom=0., top=1.):
@@ -139,7 +142,7 @@ class YoloDataset(Dataset):
             np.random.shuffle(box)
             box[:, [0, 2]] = box[:, [0, 2]] * nw / iw + dx
             box[:, [1, 3]] = box[:, [1, 3]] * nh / ih + dy
-            if flip: # 如果图片经过了翻转，数据也要相应变换
+            if flip:  # 如果图片经过了翻转，数据也要相应变换
                 box[:, [0, 2]] = w - box[:, [2, 0]]
             box[:, 0:2][box[:, 0:2] < 0] = 0
             box[:, 2][box[:, 2] > w] = w
@@ -151,12 +154,188 @@ class YoloDataset(Dataset):
 
         return img_data, box
 
+    def merge_bboxes(self, bboxes, cutx, cuty):
+        '''
+        对框框的一些后处理，结合parse_metadata_with_mosaic函数处理内容一目了然
+        '''
+        merge_bbox = []
+        for i in range(len(bboxes)):
+            for box in bboxes[i]:
+                tmp_box = []
+                x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
+
+                if i == 0:
+                    if y1 > cuty or x1 > cutx:
+                        continue
+                    if y2 >= cuty and y1 <= cuty:
+                        y2 = cuty
+                    if x2 >= cutx and x1 <= cutx:
+                        x2 = cutx
+
+                if i == 1:
+                    if y2 < cuty or x1 > cutx:
+                        continue
+                    if y2 >= cuty and y1 <= cuty:
+                        y1 = cuty
+                    if x2 >= cutx and x1 <= cutx:
+                        x2 = cutx
+
+                if i == 2:
+                    if y2 < cuty or x2 < cutx:
+                        continue
+                    if y2 >= cuty and y1 <= cuty:
+                        y1 = cuty
+                    if x2 >= cutx and x1 <= cutx:
+                        x1 = cutx
+
+                if i == 3:
+                    if y1 > cuty or x2 < cutx:
+                        continue
+                    if y2 >= cuty and y1 <= cuty:
+                        y2 = cuty
+                    if x2 >= cutx and x1 <= cutx:
+                        x1 = cutx
+                tmp_box.append(x1)
+                tmp_box.append(y1)
+                tmp_box.append(x2)
+                tmp_box.append(y2)
+                tmp_box.append(box[-1])
+                merge_bbox.append(tmp_box)
+        return merge_bbox
+
     def parse_metadata_with_mosaic(self, index, jitter=0.3, hue=0.1, sat=0.7, val=0.4):
-        pass
+        '''
+
+        :param index: 当前操作的数据的下标
+        :param jitter: 随机缩放的最大比例
+        :param hue、sat、val: 色域变换的参数
+        :return: img_data, box
+        '''
+
+        # 随机取出三条数据，和当前数据混在一起
+        datas = sample(self.dataset, 3)
+        datas.append(self.dataset[index])
+        shuffle(datas)
+
+        # 初始化一些参数，准备好摆放数据的位置
+        h, w = HP.input_shape
+        min_offset_x = self.rand(0.3, 0.7)
+        min_offset_y = self.rand(0.3, 0.7)
+        img_datas = []
+        box_datas = []
+
+        for i, data in enumerate(datas):
+            data = data.split()
+            img_path, labels = data[0], data[1:]
+
+            # 读取到图像，并确保其为RGB格式的数据
+            img = Image.open(img_path)
+            img = cvtColor(img)
+            iw, ih = img.size
+
+            # 得到最原始的预测框数据
+            box = np.array([np.array(list(map(int, box.split(',')))) for box in labels])
+
+            # 翻转图像
+            flip = self.rand() < 0.5
+            if flip and len(box) > 0:
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                box[:, [0, 2]] = iw - box[:, [2, 0]]
+
+            # 图片缩放、长宽扭曲
+            new_ar = iw / ih * self.rand(1 - jitter, 1 + jitter) / self.rand(1 - jitter, 1 + jitter)
+            scale = self.rand(0.4, 1.)
+            if new_ar < 1:
+                nh = int(scale * h)
+                nw = int(nh * new_ar)
+            else:
+                nw = int(scale * w)
+                nh = int(nw / new_ar)
+            img = img.resize((nw, nh), Image.BICUBIC)
+
+            # 根据此时图片在数据列表中的位置，确定图片位置
+            global dx, dy
+            if i == 0:
+                dx = int(w * min_offset_x) - nw
+                dy = int(h * min_offset_y) - nh
+            elif i == 1:
+                dx = int(w * min_offset_x) - nw
+                dy = int(h * min_offset_y)
+            elif i == 2:
+                dx = int(w * min_offset_x)
+                dy = int(h * min_offset_y)
+            elif i == 3:
+                dx = int(w * min_offset_x)
+                dy = int(h * min_offset_y) - nh
+
+            # 给图像的边缘上加上灰条，以填充满预期尺寸的图片
+            new_img = Image.new('RGB', (w, h), (128, 128, 128))
+            new_img.paste(img, (dx, dy))
+            img_data = np.array(new_img.__array__(), np.uint8)
+
+            # 对预测框的数据进行相应的调整
+            # box_data = []
+            if len(box) > 0:
+                np.random.shuffle(box)
+                box[:, [0, 2]] = box[:, [0, 2]] * nw / iw + dx
+                box[:, [1, 3]] = box[:, [1, 3]] * nh / ih + dy
+                box[:, 0:2][box[:, 0:2] < 0] = 0
+                box[:, 2][box[:, 2] > w] = w
+                box[:, 3][box[:, 3] > h] = h
+                box_w = box[:, 2] - box[:, 0]
+                box_h = box[:, 3] - box[:, 1]
+                # 剔除无效数据
+                box = box[np.logical_and(box_w > 1, box_h > 1)]
+                # box_data = np.zeros((len(box), 5))
+                # box_data[:len(box)] = box
+
+            img_datas.append(img_data)
+            # box_datas.append(box_data)
+            box_datas.append(box)
+
+        # 将图片分割，放在一起
+        cutx = int(w * min_offset_x)
+        cuty = int(h * min_offset_y)
+
+        new_img = np.zeros([h, w, 3])
+        new_img[:cuty, :cutx, :] = img_datas[0][:cuty, :cutx, :]
+        new_img[cuty:, :cutx, :] = img_datas[1][cuty:, :cutx, :]
+        new_img[cuty:, cutx:, :] = img_datas[2][cuty:, cutx:, :]
+        new_img[:cuty, cutx:, :] = img_datas[3][:cuty, cutx:, :]
+        img_data = np.array(new_img, np.uint8)
+
+        # 即将进行色域变换，计算下色域变换的参数
+        r = np.random.uniform(-1, 1, 3) * [hue, sat, val] + 1
+        # 将图像的色域转换到HSV上
+        hue, sat, val = cv2.split(cv2.cvtColor(img_data, cv2.COLOR_RGB2HSV))
+        dtype = img_data.dtype
+        # 应用变换
+        x = np.arange(0, 256, dtype=r.dtype)
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+        img_data = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
+        img_data = cv2.cvtColor(img_data, cv2.COLOR_HSV2RGB)
+
+        # 把框调整、整合一下
+        box = self.merge_bboxes(box_datas, cutx, cuty)
+
+        return img_data, box
+
 
 if __name__ == '__main__':
     dataset = YoloDataset('testset.txt')
 
-    img_data, box = dataset.parse_metadata(0, random=True)
-    print(img_data)
-    print(box)
+    for i in range(10):
+        img_data, box = dataset.parse_metadata_with_mosaic(i)
+        print(img_data.shape)
+        img_data = cv2.cvtColor(img_data, cv2.COLOR_RGB2BGR)
+
+        for b in box:
+            print(b)
+            cv2.rectangle(img_data, (b[0], b[1]), (b[2], b[3]), color=(0, 0, 255), thickness=1)
+
+        cv2.namedWindow('123')
+        cv2.imshow('123', img_data)
+        cv2.waitKey(0)
+
